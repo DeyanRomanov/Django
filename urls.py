7cd1db212693aa394a8322dca854222fdd630f6c
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-02-28 20:59:08 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,10 @@
-from django.urls import path
-
-from OnlineLibrary.library.views import home_page, add_book_page, edit_book_page, details_book_page, profile_page, \
-    edit_profile_page, delete_profile_page, create_profile_page, delete_book
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('admin/', admin.site.urls),
+    path('', include('OnlineLibrary.library.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    path('add/', add_book_page, name='add book'),
-    path('edit/<int:pk>/', edit_book_page, name='edit book'),
-    path('details/<int:pk>/', details_book_page, name='details book'),
-    path('delete/<int:pk>/', delete_book, name='delete book'),
-
-    path('profile/', profile_page, name='profile page'),
-    path('profile/edit/', edit_profile_page, name='edit profile'),
-    path('profile/delete/', delete_profile_page, name='delete profile'),
-    path('profile/create/', create_profile_page, name='create profile'),
-
-]
