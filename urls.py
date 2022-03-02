89eb732302aa3c14cd6485eff889e455f9b536c3
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-03-02 13:00:00 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from musciapp.main.views import profile_delete, profile_details, album_delete, album_edit, album_details, add_album, \
+    home_page, profile_create
 
 urlpatterns = [
-            path('admin/', admin.site.urls),
-            path('', include('musciapp.main.urls'))
-            ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', home_page, name='home page'),
+    path('profile/details/', profile_details, name='profile details page'),
+    path('profile/delete/', profile_delete, name='delete profile page'),
+    path('profile/create/', profile_create, name='create profile page'),
+
+    path('album/add/', add_album, name='add album page'),
+    path('album/details/<int:pk>', album_details, name='album details page'),
+    path('album/edit/<int:pk>/', album_edit, name='edit album page'),
+    path('album/delete/<int:pk>', album_delete, name='delete album page'),
+]
