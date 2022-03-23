69d3810f151225a960b62ede85d6ebe6fe8cff94
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-03-23 15:07:50 +0200

    Update settings.py

--- Petstagram_first/Petstagram/settings.py
+++ Petstagram_first/Petstagram/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x4@1q#sql%6w(l54jv@tma49q2$j4ee7123@i12w(qe*ijg*+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
