c8d48cee7ea5843b7943ebf113d927926cc3c6ea
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-03-23 15:05:02 +0200

    Update settings.py

--- trainingProject/trainingProject/settings.py
+++ trainingProject/trainingProject/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aolgt)7x__n=#eo7xhy)4q#$=wd3#y%*i7q+jou)*^u#wt8%uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
