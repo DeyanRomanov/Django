949c3dfbe4f063b02661aaf4909fa69b3984372b
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-03-23 15:06:30 +0200

    Update settings.py

--- Recipes/Recipes/settings.py
+++ Recipes/Recipes/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5#imm55#0!5ymf3h_9&0pi@c0mw8p*&^+!h4vtpxj=cgv*+5ae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,16 +74,7 @@ WSGI_APPLICATION = 'Recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipes_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Snejieli90@',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
